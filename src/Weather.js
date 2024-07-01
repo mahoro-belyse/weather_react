db17f7e3ca25bf2465434704706946bcc988bce7
mahoro-belyse <151196672+mahoro-belyse@users.noreply.github.com>
2024-07-01 16:21:41 +0300

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,13 @@
 import React from "react";
-alert("welcome to the page!!");
+import axios from "axios";
+
 export default function Weather() {
+  function HandleResponse(response) {
+    alert(`the weather in New york is ${response.data.main.temp}&deg;c`);
+  }
+  let apiKey = `d860d36baeo33ebcafd4ec2d01tf4406`;
+  let apiurl = `https://api.shecodes.io/weather/v1/current?query=New york&key=${apiKey}&units=metric`;
+  axios.get(apiurl).then(HandleResponse);
   return (
     <div>
       <h1>hello sirr</h1>
